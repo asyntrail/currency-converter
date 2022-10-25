609fcfd7159c1e420f91d36a7e95996ae39e578b
asyntrail <asyntrail@gmail.com>
2022-10-25 20:10:40 +0300

    ad src,public create simpe form, add dada json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import cur from './data/currency.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="app">
+        {cur.map( item => 
+        <p>UAH {item.uah} to USD {item.usd} </p>)}
+        <input type="text" />
+        <button>Convert</button>
+      </div>
     </div>
   );
 }
