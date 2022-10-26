f538a6fb747b2658e0fc73f946f4c72d05ba1e6a
asyntrail <asyntrail@gmail.com>
2022-10-26 19:41:24 +0300

    first test convert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './index.css';
 import cur from './data/currency.json';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [inputstate, setInputstate] = useState('');
+  const [result, setResult] = useState(0);
+
+  const currency = cur[0]["usd"];
+
   return (
     <div className="container">
       <div className="app">
-        {cur.map( item => 
-        <p>UAH {item.uah} to USD {item.usd} </p>)}
-        <input type="text" />
-        <button>Convert</button>
+        {cur.map(item =>
+          <p key={item+"key"}>UAH {item.uah} to USD {item.usd} </p>)}
+        <input onChange={(event) => {setInputstate(event.target.value) }} type="number" />
+        <button onClick={() =>
+          setResult(inputstate * currency)}>Convert</button>
+        <p>Result is {result} </p>
       </div>
     </div>
   );
