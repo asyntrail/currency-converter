2f30dd32b1ee71069425b14fd09a9c02325c8ca0
asyntrail <asyntrail@gmail.com>
2023-02-04 21:50:20 +0200

    first test

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ import React, { useState } from 'react';
 function App() {
   const [inputstate, setInputstate] = useState('');
   const [result, setResult] = useState(0);
+  const [caption, setCaption] = useState(true);
 
-  const usdinfo = info[0]["sale"];
+  const usdinfo = (Math.ceil((info[0]["sale"])* 100) / 100);
+
+
 
   return (
     <div className="container">
-
       <div className="app">
-      <form onSubmit={(event) => {setResult(inputstate * usdinfo)
+      <form onSubmit={(event) => {caption? setResult(inputstate * usdinfo): setResult(inputstate / usdinfo)
       event.preventDefault();}
       }>
-        <h3>Калькулятор</h3>
-          <input onChange={(event) => { setInputstate(event.target.value) }}
-           type="number" placeholder="Сума в гривні" name="maininput" />
+        <h3>Ціна за 1$ по курсу Приват {usdinfo}</h3>
+        <div className="input-row">
+        <input onChange={(event) => { setInputstate(event.target.value) }}
+         type="number" placeholder={caption?"Сума в доларах":"Сума в гривні"}  name="maininput" /> 
+         <button type="button"  onClick={() => {setCaption(!caption);setResult(0);}} >Переключити</button>
+        </div>
         <button type="submit" >Порахувати</button>
       </form>
-        <p>Сума в доларах {result} </p>
+        <p>{caption?"Сума в гривні":"Сума в доларах"} {result.toFixed(2)} {caption?"ГРН":"$"} </p>
       </div>
     </div>
   );
